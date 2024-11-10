337de89a106b02d84e987cb76783f20f37a4dc66
Guspaz <guspaz@gmail.com>
2024-11-10 01:59:53 -0500

    Fix StatusDaemon due to missing loop

--- StatusDaemon.cs
+++ StatusDaemon.cs
@@ -15,28 +15,31 @@ namespace rt4k_pi
         {
             Task.Run(async () =>
             {
-                Hostname = Util.RunCommand("hostname").ToLower();
-
-                try
+                while (!cts.IsCancellationRequested)
                 {
-                    string[] fields = Util.RunCommand("nmcli", "-t -c no -f active,ssid,chan,rate,signal dev wifi").Split('\n').Where(l => l.StartsWith("yes:")).First().Split(':');
+                    Hostname = Util.RunCommand("hostname").ToLower();
 
-                    WifiStatus = new Dictionary<string, string>
+                    try
                     {
-                        {"active", fields[0]},
-                        {"ssid", fields[1]},
-                        {"chan", fields[2]},
-                        {"rate", fields[3]},
-                        {"signal", fields[4]},
-                        {"quality", Convert.ToInt32(fields[4]) switch { > 80 => "great", > 55 => "good", > 30 => "weak", > 5 => "bad", _ => "terrible" } }
-                    };
-                }
-                catch
-                {
-                    WifiStatus = new Dictionary<string, string> { { "active", "no" }, { "ssid", "" } };
-                }
+                        string[] fields = Util.RunCommand("nmcli", "-t -c no -f active,ssid,chan,rate,signal dev wifi").Split('\n').Where(l => l.StartsWith("yes:")).First().Split(':');
 
-                await Task.Delay(UPDATE_INTERVAL, cts.Token);
+                        WifiStatus = new Dictionary<string, string>
+                        {
+                            {"active", fields[0]},
+                            {"ssid", fields[1]},
+                            {"chan", fields[2]},
+                            {"rate", fields[3]},
+                            {"signal", fields[4]},
+                            {"quality", Convert.ToInt32(fields[4]) switch { > 80 => "great", > 55 => "good", > 30 => "weak", > 5 => "bad", _ => "terrible" } }
+                        };
+                    }
+                    catch
+                    {
+                        WifiStatus = new Dictionary<string, string> { { "active", "no" }, { "ssid", "" } };
+                    }
+
+                    await Task.Delay(UPDATE_INTERVAL, cts.Token);
+                }
             }, cts.Token);
         }
     }
